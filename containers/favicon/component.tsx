eb7daef84a0ed7402a2f3b198aaccf5401e39c82
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-04-20 22:16:18 +0200

    Favicon

--- containers/favicon/component.tsx
+++ containers/favicon/component.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC } from 'react';
 
 import Head from 'next/head';
 
@@ -6,30 +6,40 @@ interface FaviconProps {
 }
 
 export const Favicon: FC<FaviconProps> = () => {
-  const [theme, setTheme] = useState('light');
+  // const [theme, setTheme] = useState('light');
 
-  useEffect(() => {
-    // Check if you are using dark mode
-    window
-      .matchMedia('(prefers-color-scheme: dark)')
-      .addEventListener('change', (e) => {
-        if (e.matches) {
-          setTheme('dark');
-        } else {
-          setTheme('light');
-        }
-      });
-  }, []);
+  // useEffect(() => {
+  //   // Check if you are using dark mode
+  //   window
+  //     .matchMedia('(prefers-color-scheme: dark)')
+  //     .addEventListener('change', (e) => {
+  //       if (e.matches) {
+  //         setTheme('dark');
+  //       } else {
+  //         setTheme('light');
+  //       }
+  //     });
+  // }, []);
 
   return (
     <Head>
-      {theme === 'light' && (
-        <link rel="icon" href="/favicon-light.svg" />
-      )}
-
-      {theme === 'dark' && (
-        <link rel="icon" href="/favicon-dark.svg" />
-      )}
+      <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+      <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+      <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+      <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+      <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+      <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+      <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+      <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <meta name="msapplication-TileColor" content="#ffffff" />
+      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 };
