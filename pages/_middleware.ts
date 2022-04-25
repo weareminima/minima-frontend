3ffefedd8524721f398ce0d164869319e6144ff5
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-04-25 22:46:44 +0200

    Remove redirection in preview URLs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 
 export async function middleware(req) {
   const { pathname } = req.nextUrl;
-  if (process.env.NODE_ENV === 'production' && pathname === '/') {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production' && pathname === '/') {
     return NextResponse.redirect('/coming-soon');
   }
   return NextResponse.next();
