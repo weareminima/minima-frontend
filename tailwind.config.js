7fcd22c4ceae36098438f2be7faff3972ace766f
Miguel Barrenechea Sánchez <miguel.barrenechea@vizzuality.com>
2022-06-13 02:04:41 +0200

    Line height

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
     fontSize: {
       xs: ['0.8125rem', { lineHeight: '1.25rem' }],
       sm: ['0.9375rem', { lineHeight: '1.375rem' }],
-      base: ['1.0625rem', { lineHeight: '1.5rem' }],
+      base: ['1.0625rem', { lineHeight: '1.625rem' }],
       lg: ['1.1875rem', { lineHeight: '1.5rem' }],
       xl: ['1.375rem', { lineHeight: '1.5rem' }],
       '2xl': ['1.5rem', { lineHeight: '1.75rem' }],
